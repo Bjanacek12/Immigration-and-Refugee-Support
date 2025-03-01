b57154298f2b31c50b4716cf88e01b22550d3e74
BrianJanacek <brian@janacek.us>
2025-03-01 13:21:56 -0500

    merge

--- Backend/Text-Speech.py
+++ Backend/Text-Speech.py
@@ -1,11 +1,20 @@
+import requests
+import json
 from pathlib import Path
 from openai import OpenAI
 
-client = OpenAI()
+textInfo_URL = ""
+textResponse = requests.get(textInfo_URL)
+with open(textResponse.json(), "r") as file:
+    data = json.load(file)
+textInput = json.dumps(data)
+
+
+client = OpenAI(key)
 speech_file_path = Path(__file__).parent / "speech.mp3"
 response = client.audio.speech.create(
-    model="tts-1",
-    voice="alloy",
-    input="Today is a wonderful day to build something people love!",
+    model ="tts-1",
+    voice ="ash",
+    input = textInput,
 )
 response.stream_to_file(speech_file_path)
